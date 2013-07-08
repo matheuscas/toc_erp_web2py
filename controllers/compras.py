24f965406f0eab75a15e481c15a4d64dde4301a8
matheuscas <matheus.mcas@gmail.com>
2013-07-08 18:07:15 -0300

    acrescentado o registro de contato ao form do fornecedor

--- controllers/compras.py
+++ controllers/compras.py
@@ -1,10 +1,16 @@
 
 def inserir_fornecedor():
-	form = SQLFORM.factory(db.fornecedor, db.endereco)
+	form = SQLFORM.factory(db.fornecedor, db.endereco, db.contato)
 	if form.process().accepted:
+		
 	   id = db.endereco.insert(**db.endereco._filter_fields(form.vars))	
 	   form.vars.endereco = id
+
+	   id = db.contato.insert(**db.contato._filter_fields(form.vars))
+	   form.vars.contato_id = id
+
 	   id = db.fornecedor.insert(**db.fornecedor._filter_fields(form.vars))
+
 	   response.flash = 'Registro inserido com sucesso'
 	return dict(form=form)   
 
