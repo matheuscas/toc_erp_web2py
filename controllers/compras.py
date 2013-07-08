addfa364aed981daf421b8b8e8067b9c09fb7729
matheuscas <matheus.mcas@gmail.com>
2013-07-08 12:04:02 -0300

    controller do modulo de compras

---
+++ controllers/compras.py
@@ -0,0 +1,16 @@
+
+def inserir_fornecedor():
+	form = SQLFORM.factory(db.fornecedor, db.endereco)
+	if form.process().accepted:
+	   id = db.endereco.insert(**db.endereco._filter_fields(form.vars))	
+	   form.vars.endereco = id
+	   id = db.fornecedor.insert(**db.fornecedor._filter_fields(form.vars))
+	   response.flash = 'FOI'
+	return dict(form=form)   
+
+def atualizar_fornecedor():
+	pass
+
+def listar_fornecedores():	
+	pass
+	
