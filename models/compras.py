ac84d11d8707e8b27ca37989ae4ff9edaa81957d
matheuscas <matheus.mcas@gmail.com>
2013-07-08 12:02:23 -0300

    atualizacao do model de compras do prototipo

--- models/compras.py
+++ models/compras.py
@@ -4,27 +4,28 @@ db.define_table('endereco',
 	Field('bairro','string',required=True, notnull=True),
 	Field('cidade','string',required=True, notnull=True),
 	Field('estado','string',required=True, notnull=True),
-	Field('cep','string',requires=IS_LENGTH(8)),
-	format=lambda f: f.rua + '/' + f.bairro)
+	Field('cep','string',length=8),
+	format='%(rua), %(numero), %(bairro)')
 
-db.define_table('pessoaContato',
-	Field('nome','string'),
-	Field('telefone','string'),
-	format=lambda f: f.nome + ':' + f.telefone)
+db.define_table('contato',
+	Field('nome_contato','string'),
+	Field('telefone_contato','string'), 
+	format='%(nome_contato) - %(telefone_contato)')
 
 db.define_table('fornecedor',
 	Field('nome','string',required=True, notnull=True),
 	Field('nomeFantasia','string'),
-	Field('cpf_cnpj','integer',required=True,notnull=True,unique=True),
-	Field('endereco_id','reference endereco',required=True,notnull=True),
+	Field('cpf_cnpj','string',required=True,notnull=True,unique=True, length=14),
+	Field('endereco','reference endereco',writable=False, readable=False),
 	Field('email','string'),
 	Field('telefone','string'),
-	Field('contato_id','reference pessoaContato'),
+	Field('contato_id','reference contato', writable=False, readable=False),
 	Field('historico','text'),
-	format=lambda f: f.nome + '-' + f.cpf_cnpj)
+	format='%(nomeFantasia) - %(cpf_cnpj)')
 
 db.define_table('ramoAtividade',
-	Field('nome','string',required=True, notnull=True),
+	Field('nome_ramo','string',required=True, notnull=True),
 	Field('descricao','text'),
-	Field('forncedor_id','reference fornecedor'),
-	format= lambda f: f.nome + '(s)')
+	Field('forncedor_id','reference fornecedor', writable=False, readable=False),
+	auth.signature,
+	format='%(nome_ramo)')
