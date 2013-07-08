76a1fe8e1362a79c0301b0e639f8ab7e3a3126d7
matheuscas <matheus.mcas@gmail.com>
2013-07-08 16:38:25 -0300

    alterada mensagem de sucesso ao inserir um fornecedor

--- controllers/compras.py
+++ controllers/compras.py
@@ -5,7 +5,7 @@ def inserir_fornecedor():
 	   id = db.endereco.insert(**db.endereco._filter_fields(form.vars))	
 	   form.vars.endereco = id
 	   id = db.fornecedor.insert(**db.fornecedor._filter_fields(form.vars))
-	   response.flash = 'FOI'
+	   response.flash = 'Registro inserido com sucesso'
 	return dict(form=form)   
 
 def atualizar_fornecedor():
