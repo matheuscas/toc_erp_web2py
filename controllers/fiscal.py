d0148ce9e1a77da4ee873b0fb72d6fc366731055
matheuscas <matheus.mcas@gmail.com>
2013-07-12 17:11:48 -0300

    inclusao de uma funcao que valida a chave unica "tipo do imposto,aliquota e percentual"

--- controllers/fiscal.py
+++ controllers/fiscal.py
@@ -1,3 +1,5 @@
 def inserir_imposto():
-	return dict(form=crud.create(db.imposto))
+	form=crud.create(db.imposto, 
+		onvalidation=valida_trinca_tipo_imposto_tipo_aliquota_e_percentual_imposto)
+	return dict(form=form)
 	
