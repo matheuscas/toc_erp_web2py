fd71046b5e8ad5222dee9043ca5c5d45b4085a69
matheuscas <matheus.mcas@gmail.com>
2013-07-15 12:01:49 -0300

    adicionado as funcoes de pesquisar e atualizar imposto

--- controllers/fiscal.py
+++ controllers/fiscal.py
@@ -2,4 +2,14 @@ def inserir_imposto():
 	form=crud.create(db.imposto, 
 		onvalidation=valida_trinca_tipo_imposto_tipo_aliquota_e_percentual_imposto)
 	return dict(form=form)
+
+def pesquisar_imposto():
+	form=SQLFORM.grid(db.imposto)
+	return dict(form=form)
+
+def atualizar_imposto():
+	#crud.settings.update_deletable = False
+	#form = crud.update(db.imposto,request.args(0))
+	form = SQLFORM(db.imposto,request.args(0))
+	return dict(form=form)		
 	
