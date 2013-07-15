0cd6dbcb9f604c82afc13e1f451264607c1b324d
matheuscas <matheus.mcas@gmail.com>
2013-07-15 19:34:12 -0300

    adicionada suite de testes do estoque

--- tests/completeTestSuite.py
+++ tests/completeTestSuite.py
@@ -4,11 +4,14 @@ from testCadastroFornecedor import TestCadastroFornecedor
 from testCadastroRamoAtividade import TestCadastroRamoAtividade
 from testSuiteCompras import suite_compras
 from testSuiteFiscal import suite_fiscal
+from testSuiteEstoque import suite_estoque
 
 completeSuite = unittest.TestSuite()
 suiteCompras = suite_compras()
 suiteFiscal = suite_fiscal()
+suiteEstoque = suite_estoque()
 completeSuite.addTests(suiteCompras)
 completeSuite.addTests(suiteFiscal)
+completeSuite.addTests(suiteEstoque)
 
 unittest.TextTestRunner(verbosity=2).run(completeSuite) 
