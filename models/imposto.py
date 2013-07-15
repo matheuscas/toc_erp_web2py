01c0e8ba64041d609d81a8e9fb17bf44a240c72d
matheuscas <matheus.mcas@gmail.com>
2013-07-15 12:01:03 -0300

    alterado o valor padrao para listas para None. Se deixar algum valor padrao nao aparece o valor correto no form de atualizacao. Como n sei ainda como resolver, deixei o simples mesmo.

--- models/imposto.py
+++ models/imposto.py
@@ -7,10 +7,12 @@ db.define_table('imposto',
 	format= '%(nome_imposto), %(tipo_imposto), %(tipo_aliquota_imposto), %(percentual_imposto)')
 
 db.imposto.nome_imposto.requires = [IS_NOT_EMPTY(),IS_UPPER(),CLEANUP(),IS_NOT_IN_DB(db, 'imposto.nome_imposto')]
-db.imposto.tipo_imposto.requires = IS_IN_SET(['IPI','ICMS','PIS','COFINS'],zero=T('Choose a type'))
+db.imposto.tipo_imposto.requires = IS_IN_SET(['IPI','ICMS','PIS','COFINS'],zero=None)
 db.imposto.tipo_aliquota_imposto.requires= IS_IN_SET(['Isento','Substituido', 'Tributado', 'Nao Tributado'],
-	zero=T('Choose an aliquot'))
-db.imposto.situacao_imposto.requires = IS_IN_SET(['ATIVO','INATIVO'],zero=T('Choose a status'))
+	zero=None)
+db.imposto.situacao_imposto.requires = IS_IN_SET(['ATIVO','INATIVO'],zero=None)
+db.imposto.percentual_imposto.requires = IS_FLOAT_IN_RANGE(0, 100, dot=".", 
+	error_message='enter a number between 0 and 100')
 
 def valida_trinca_tipo_imposto_tipo_aliquota_e_percentual_imposto(form):
 	print form.vars.tipo_imposto, form.vars.tipo_aliquota_imposto, form.vars.percentual_imposto
