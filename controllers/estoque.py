fedabd65d40327315ea1df49b0caa7ab18fe98e2
matheuscas <matheus.mcas@gmail.com>
2013-07-15 19:37:16 -0300

    inseridas funcoes de pesquisa e alteracao de embalagens

--- controllers/estoque.py
+++ controllers/estoque.py
@@ -1,3 +1,12 @@
 def inserir_embalagem():
-	form = crud.create(db.embalagem)
+	form = crud.create(db.embalagem, onvalidation=valida_unidade_medida_e_quantidade_casas_decimais)
 	return dict(form=form)
+
+def pesquisar_embalagens():
+	form = SQLFORM.grid(db.embalagem)
+	return dict(form=form)
+
+def atualizar_embalagem():
+	#form = SQLFORM(db.embalagem, request.args(0))
+	form = SQLFORM(db.embalagem,request.args(0))
+	return dict(form=form)	
