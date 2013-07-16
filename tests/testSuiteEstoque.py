fdedf3dcab6cdf226c1a10dc789d78e2d6638bae
matheuscas <matheus.mcas@gmail.com>
2013-07-16 11:07:47 -0300

    insercao da suite do cadastro de secao

--- tests/testSuiteEstoque.py
+++ tests/testSuiteEstoque.py
@@ -1,10 +1,12 @@
 import unittest
 from testCadastroEmbalagem import TestCadastroEmbalagem
+from testCadastroSecao import TestCadastroSecao
 
 def suite_estoque():
 	suite = unittest.TestSuite()
 	suite_embalagem = TestCadastroEmbalagem.suite()
+	suite_secao = TestCadastroSecao.suite()
 	suite.addTest(suite_embalagem)
+	suite.addTest(suite_secao)
 	return suite
-
-unittest.TextTestRunner(verbosity=2).run(suite_estoque())	
+	
