8fd743125d68a11e1436b672b8f79a3ab6652603
matheuscas <matheus.mcas@gmail.com>
2013-07-16 19:13:34 -0300

    inclusao da suite de teste do cadastro de subsecoes

--- tests/testSuiteEstoque.py
+++ tests/testSuiteEstoque.py
@@ -1,12 +1,15 @@
 import unittest
 from testCadastroEmbalagem import TestCadastroEmbalagem
 from testCadastroSecao import TestCadastroSecao
+from testCadastroSubsecao import TestCadastroSubsecao
 
 def suite_estoque():
 	suite = unittest.TestSuite()
 	suite_embalagem = TestCadastroEmbalagem.suite()
 	suite_secao = TestCadastroSecao.suite()
+	suite_subsecao = TestCadastroSubsecao.suite()
 	suite.addTest(suite_embalagem)
 	suite.addTest(suite_secao)
+	suite.addTest(suite_subsecao)
 	return suite
 	
