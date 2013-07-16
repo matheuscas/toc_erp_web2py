575a32a6c9a06592020bd77ac6b2035d810434fc
matheuscas <matheus.mcas@gmail.com>
2013-07-16 19:16:27 -0300

    alteracao do format da tabela. parece que quando ha somente um atributo chamado, tem de acrescentar o s

--- models/secao.py
+++ models/secao.py
@@ -2,7 +2,7 @@ db.define_table('secao',
 	Field('nome','string',required=True, notnull=True, unique=True),
 	Field('descricao','text'),
 	Field('situacao','string',required=True,notnull=True),
-	format = '%(secao)')
+	format = '%(nome)s')
 
 db.secao.nome.requires = [IS_NOT_EMPTY(),IS_UPPER(),CLEANUP(),IS_NOT_IN_DB(db, 'secao.nome')]
 db.secao.situacao.requires = IS_IN_SET(['ATIVO','INATIVO'],zero=None)
