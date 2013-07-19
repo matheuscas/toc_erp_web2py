e20d3df21c9706f48db9204647a7bcfe4ce84a5a
matheuscas <matheus.mcas@gmail.com>
2013-07-19 17:02:53 -0300

    inserida suite de testes de marca de produtos

--- tests/testSuiteEstoque.py
+++ tests/testSuiteEstoque.py
@@ -3,6 +3,7 @@ from testCadastroEmbalagem import TestCadastroEmbalagem
 from testCadastroSecao import TestCadastroSecao
 from testCadastroSubsecao import TestCadastroSubsecao
 from testCadastroFabricante import TestCadastroFabricante
+from testCadastroMarca import TestCadastroMarca
 
 def suite_estoque():
 	suite = unittest.TestSuite()
@@ -10,9 +11,11 @@ def suite_estoque():
 	suite_secao = TestCadastroSecao.suite()
 	suite_subsecao = TestCadastroSubsecao.suite()
 	suite_fabricante = TestCadastroFabricante.suite()
+	suite_marca = TestCadastroMarca.suite()
 	suite.addTest(suite_embalagem)
 	suite.addTest(suite_secao)
 	suite.addTest(suite_subsecao)
 	suite.addTest(suite_fabricante)
+	suite.addTest(suite_marca)
 	return suite
 	
