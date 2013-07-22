fff0a4fc79c2f2f7f7b4f6b5bd5c663a2a5965e6
matheuscas <matheus.mcas@gmail.com>
2013-07-22 15:53:09 -0300

    criados arquivos de model e testes do produto

---
+++ models/produto.py
@@ -0,0 +1,23 @@
+db.define_table('produto',
+	Field('nome','string',required=True, notnull=True),
+	Field('descricao','text'),
+	Field('secao_id','reference secao',required=True, notnull=True),
+	Field('subsecao_id','reference subsecao'),
+	Field('marca_id','reference marca'),
+	Field('fabricante_id','reference fabricante'),
+	Field('codigo_barras','string'),
+	Field('codigo_ncm','string'),
+	Field('unidade_medida','string', required=True, notnull=True),
+	Field('estoque_padrao_compra','float'),
+	Field('tipo_item','string'),
+	Field('cst_compra_pis','string'),
+	Field('cst_compra_cofins','string'),
+	Field('cst_compra_ipi'),
+	format='%(nome)s')
+
+db.produto.nome.requires = [IS_NOT_EMPTY(), IS_UPPER(), CLEANUP()]
+db.produto.secao_id.requires = IS_IN_DB(db, 'secao.id',zero=None)
+db.produto.subsecao_id.requires = IS_EMPTY_OR(IS_IN_DB(db, 'subsecao.id'))
+db.produto.marca_id.requires = IS_EMPTY_OR(IS_IN_DB(db,'marca.id'))
+db.produto.fabricante_id.requires = IS_EMPTY_OR(IS_IN_DB(db,'fabricante.id'))
+db.produto.estoque_padrao_compra.requires = IS_EMPTY_OR(IS_FLOAT_IN_RANGE(0,100))
---
+++ tests/testCadastroProduto.py
@@ -0,0 +1,54 @@
+import unittest
+import time
+from testSetup import TestSetup
+
+class TestCadastroProduto(TestSetup):
+	"""docstring for TestCadastroProduto"""
+	def setUp(self):
+		super(TestCadastroProduto,self).setUp()
+		compras_url_base = 'http://localhost:8000/toc_erp_web2py/estoque/'
+		controller_inserir = 'inserir_produto'
+		self.url_inserir_produto = compras_url_base + controller_inserir
+		self.limpa_dados_tabela('produto')
+		self.nome = 'Um produto qualquer'
+		self.unidade_medida = 'UN'
+		self.limpa_dados_tabela('marca')
+		self.limpa_dados_tabela('fabricante')
+		self.limpa_dados_tabela('secao')
+		self.limpa_dados_tabela('produto')
+
+
+	def test_nao_preenche_campos_obrigatorios(self):
+		self.driver.get(self.url_inserir_produto)
+		self.submit_form()
+		time.sleep(0.5)
+
+		mensagem_de_erro = 'enter a value'
+		mensagem_de_erro_2 = 'value not in database'
+
+		assert (self.driver.find_element_by_id('nome__error').text == mensagem_de_erro and
+				self.driver.find_element_by_id('unidade_medida__error').text == mensagem_de_erro and
+				self.driver.find_element_by_id('secao_id__error').text == mensagem_de_erro_2) == True
+
+	def test_preenche_campos_obrigatorios(self):		
+		temp_id = self.db_test.secao.insert(nome="secao teste".upper(),situacao='ATIVO')
+		self.db_test.commit()
+
+		time.sleep(0.1)
+
+		self.driver.get(self.url_inserir_produto)
+		self.driver.find_element_by_id('produto_nome').send_keys(self.nome)
+		self.driver.find_element_by_id('produto_unidade_medida').send_keys(self.unidade_medida)
+		self.submit_form()
+
+		time.sleep(0.5)
+
+		rows = self.db_test(self.db_test.produto.nome == self.nome.upper()).select()
+		assert (len(rows) > 0) == True
+
+	@classmethod
+	def suite(cls):
+		suite = unittest.TestSuite()
+		suite.addTest(TestCadastroProduto('test_nao_preenche_campos_obrigatorios'))
+		suite.addTest(TestCadastroProduto('test_preenche_campos_obrigatorios'))
+		return suite							
