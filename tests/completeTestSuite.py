bb67b7705e72fc5a92e486c1010b36a5de6ef7b7
matheuscas <matheus.mcas@gmail.com>
2013-07-23 11:38:04 -0300

    atualizacao do suite completa de testes com a suite do financeiro

--- tests/completeTestSuite.py
+++ tests/completeTestSuite.py
@@ -5,13 +5,16 @@ from testCadastroRamoAtividade import TestCadastroRamoAtividade
 from testSuiteCompras import suite_compras
 from testSuiteFiscal import suite_fiscal
 from testSuiteEstoque import suite_estoque
+from test_suite_financeiro import suite_financeiro
 
 completeSuite = unittest.TestSuite()
 suiteCompras = suite_compras()
 suiteFiscal = suite_fiscal()
 suiteEstoque = suite_estoque()
+suite_financeiro = suite_financeiro()
 completeSuite.addTests(suiteCompras)
 completeSuite.addTests(suiteFiscal)
 completeSuite.addTests(suiteEstoque)
+completeSuite.addTests(suite_financeiro)
 
 unittest.TextTestRunner(verbosity=2).run(completeSuite) 
