fc277b5fbc844ba7826be48e2e73a0c9608032e4
matheuscas <matheus.mcas@gmail.com>
2013-07-30 16:01:12 -0300

    adicao da classe de nota fiscal de compra

--- modules/compras.py
+++ modules/compras.py
@@ -13,6 +13,30 @@ class ItemNotaFiscalCompra(object):
 		self.quantidade_embalagem = quantidade_embalagem
 		self.aliquota_icms = aliquota_icms
 		self.total = total
+
+class NotaFiscalCompra(object):
+	"""docstring for NotaFiscalCompra"""
+	def __init__(self, numero=None,data_emissao=None,
+					data_chegada=None, natureza_operacao=None,
+					fornecedor_id=None, base_calculo_icms=None,
+					valor_icms=None, valor_ipi=None,
+					frete=None, outras=None, desconto = None,
+					condicao_pagamento_id=None, total=None):
+		self.numero=numero
+		self.data_emissao = data_emissao
+		self.data_chegada = data_chegada
+		self.natureza_operacao = natureza_operacao
+		self.fornecedor_id = fornecedor_id
+		self.base_calculo_icms = base_calculo_icms
+		self.valor_icms = valor_icms
+		self.valor_ipi = valor_ipi
+		self.frete = frete
+		self.outras = outras
+		self.desconto = desconto
+		self.condicao_pagamento_id = condicao_pagamento_id
+		self.total = total
+		
+						
 		
 		
 		
