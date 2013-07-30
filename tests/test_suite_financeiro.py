f94eb7fa2a784f3b5c3723af1b6664b365f71d33
matheuscas <matheus.mcas@gmail.com>
2013-07-30 16:05:45 -0300

    adicao da suite de testes do modulo financeiro

--- tests/test_suite_financeiro.py
+++ tests/test_suite_financeiro.py
@@ -1,8 +1,11 @@
 import unittest
 from test_cadastro_condicao_pagamento import TestCadastroCondicaoPagamento
+from test_modulo_financeiro import TestGerenteFinanceiro
 
 def suite_financeiro():
 	suite = unittest.TestSuite()
 	suite_condicao_pagamento = TestCadastroCondicaoPagamento.suite()
+	suite_modulo_financeiro_gerente_financeiro = TestGerenteFinanceiro.suite()
 	suite.addTest(suite_condicao_pagamento)
+	suite.addTest(suite_modulo_financeiro_gerente_financeiro)
 	return suite
