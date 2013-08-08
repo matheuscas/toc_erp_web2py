fde4ea445ad2e4f8adc785c02587818db2080e8e
matheuscas <matheus.mcas@gmail.com>
2013-08-08 13:44:52 -0300

    model do item de uma nota de compra

---
+++ models/produto_item_compra.py
@@ -0,0 +1,15 @@
+db.define_table('item_compra',
+	Field('produto_id','integer','reference produto',required=True, notnull=True),
+	Field('nota_fiscal_id','integer','reference nota_fiscal_compra',required=True, notnull=True)
+	Field('descricao'),
+	Field('preco_unitario','double'),
+	Field('quantidade','double'),
+	Field('unidade_compra'),
+	Field('quantidade_embalagem','double'),
+	Field('aliquota_icms','decimal(4,2)'),
+	Field('total_item','double'),
+	format='%(produto_id)s %(descricao)s')
+
+#db.item_compra.produto_id.widget = SQLFORM.widgets.autocomplete(request,
+	#db.produto.nome, id_field=db.produto.id)
+db.item_compra.produto_id.requires = [IS_NOT_EMPTY()]
