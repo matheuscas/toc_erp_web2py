7d97539471e301f94139b85ea54ec2f2aafd3a1c
matheuscas <matheus.mcas@gmail.com>
2013-08-08 13:42:14 -0300

    novos models para abarcar dependencias

---
+++ models/0_fabricante.py
@@ -0,0 +1,15 @@
+db.define_table('fabricante',
+	Field('nome','string',required=True, notnull=True),
+	Field('nome_fantasia','string'),
+	Field('cpf_cnpj','string', required=True, notnull=True, unique=True),
+	Field('inscricao_estadual','string'),
+	Field('inscricao_municipal','string'),
+	Field('telefone','string'),
+	Field('email','string'),	
+	Field('site','string'),
+	Field('observacao','text'),
+	Field('situacao','string'),
+	format="%(nome)s %(cpf_cnpj)s")
+
+db.fabricante.situacao.requires = IS_IN_SET(['ATIVO','INATIVO','BLOQUEADO'],zero=None)				
+	
---
+++ models/0_secao.py
@@ -0,0 +1,8 @@
+db.define_table('secao',
+	Field('nome','string',required=True, notnull=True, unique=True),
+	Field('descricao','text'),
+	Field('situacao','string',required=True,notnull=True),
+	format = '%(nome)s')
+
+db.secao.nome.requires = [IS_NOT_EMPTY(),IS_UPPER(),CLEANUP(),IS_NOT_IN_DB(db, 'secao.nome')]
+db.secao.situacao.requires = IS_IN_SET(['ATIVO','INATIVO'],zero=None)
